b4eef52cea007050070ac750c4c8fa2e334c087d
baozhong <zhangbaozhong@boco.com.cn>
2020-11-30 15:04:54 +0800

    新建Person类

---
+++ src/main/java/com/zbz/pojo/Person.java
@@ -0,0 +1,16 @@
+package com.zbz.pojo;
+
+import com.sun.org.apache.xerces.internal.impl.dv.xs.DayDV;
+
+import java.util.Date;
+
+public class Person {
+
+    private Long id;
+    private String name;
+    private Date birthday;
+
+    public void speak(String something) {
+        System.out.println(this.name + "say:" + something);
+    }
+}
